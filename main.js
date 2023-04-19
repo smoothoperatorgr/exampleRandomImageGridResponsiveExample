4a89190fbd9e573c517ad8047cd70789183f1747
SmoothOperatorGR <smoothoperatorgr@gmail.com>
2023-04-19 14:11:14 +0300

    First Commit of the Random Image Grid example!

--- main.js
+++ main.js
@@ -1,26 +1,71 @@
-// function to get a random image URL
-function getRandomImageURL() {
-    const width = Math.floor(Math.random() * 400) + 200; // get a random width between 200 and 599
-    const height = Math.floor(Math.random() * 400) + 200; // get a random height between 200 and 599
-    return `https://picsum.photos/${width}/320`;
+// function to get a random image URL from Lorem Picsum
+function getRandomImageURLFromLoremPicsum() {
+    const width = Math.floor(Math.random() * 440) + 200; // get a random width between 200 and 599
+    const height = Math.floor(Math.random() * 440) + 200; // get a random height between 200 and 599
+    return `https://picsum.photos/${width}/${height}`;
   }
   
+  // function to get a random image URL from Pexels
+  async function getRandomImageURLFromPexels(i, apiKey) {
+    
+    let keywords = ['landscape', 'city', 'nature', 'mountains', 'sea', 'ocean', 'pattern',
+    'night', 'summer', 'winter', 'travel', 'beach', 'abstract', 'universe',
+    'snow', 'road', 'river', 'sky', 'blur', 'stars', 'streets', 'sunset',
+    'forest', 'rain', 'light', 'abstract', 'macro', 'art', 'design'];
+    
+      let keyword = keywords[Math.floor(Math.random() * keywords.length)];
+      let url = 'https://api.pexels.com/v1/search?query=' + keyword + '&per_page=16&page=';
+    const response = await fetch(url, {
+      headers: {
+        Authorization: apiKey
+      }
+    });
+    const data = await response.json();
+    let j = Math.floor(Math.random() * 16);
+    return data.photos[j].src.medium;
+  }
+  
+
   // function to create a new image grid
-  function createImageGrid() {
+  async function createImageGrid(imageSource, apiKey) {
     const container = document.getElementById('image-grid-container');
     container.innerHTML = ''; // clear the container
-    for (let i = 0; i < 16; i++) {
-      const imageURL = getRandomImageURL();
-      const imageElement = document.createElement('img');
+    let getRandomImageURL;
+    let i;
+    switch (imageSource) {
+      case 'lorem-picsum':
+        getRandomImageURL = getRandomImageURLFromLoremPicsum;
+        break;
+      case 'pexels':
+        getRandomImageURL = () => getRandomImageURLFromPexels(i,apiKey);
+        break;
+      default:
+        getRandomImageURL = getRandomImageURLFromLoremPicsum;
+        break;
+    }
+    for (i = 0; i < 16; i++) {
+      let imageURL = await getRandomImageURL(i);
+      let imageElement = document.createElement('img');
       imageElement.src = imageURL;
       container.appendChild(imageElement);
     }
   }
   
   // create a new image grid when the button is clicked
-  const button = document.getElementById('new-images-button');
-  button.addEventListener('click', createImageGrid);
-  
-  // create the initial image grid
-  createImageGrid();
+  const button = document.getElementById('go-button');
+  button.addEventListener('click', () => {
+    const imageSource = document.getElementById('image-source').value;
+    const apiKey = '<your-pexel-api-key>';
+    switch (imageSource) {
+      case 'lorem-picsum':
+        createImageGrid(imageSource);
+        break;
+      case 'pexels':
+        createImageGrid(imageSource, apiKey);
+        break;
+      default:
+        createImageGrid(imageSource);
+        break;
+    }
+});
   
